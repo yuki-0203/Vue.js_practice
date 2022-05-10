50524803aa5b2855a4c680c3a0cd64de24ee03a3
yuki <ishidayuki2150@gmail.com>
2022-05-10 17:48:36 +0900

    5-51

--- base/js/main.js
+++ base/js/main.js
@@ -1,6 +1,11 @@
 const app = Vue.createApp({
   data: () => ({
- 
-  })
+    message: 'Hello Vue.js'
+  }),
+  computed: {
+    reversedMessage: function(){
+      return this.message.split('').reverse().join('')
+    }
+  }
  })
  app.mount('#app')
